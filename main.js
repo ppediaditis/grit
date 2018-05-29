eace37590ceba0e58658f3854bc2ff56b0125385
Panagiotis Pediaditis <panagiotis.pediaditis@citrix.com>
2018-05-29 23:36:08 +0300

    Adding node to d3

--- main.js
+++ main.js
@@ -3,36 +3,62 @@
 
 var vis = d3.select("#graph")
             .append("svg")
-.attr("width", 200).attr("height", 200);
+.attr("width", 500).attr("height", 500);
 
 var nodes = [
-    {x: 10, y: 50},
+    {x: 10, y: 90},
     {x: 70, y: 10},
     {x: 140, y: 50}
-  ]
+  ];
 
 
+var getLinkPositions = function(x1, y1, x2, y2, width, height) {
+	return [
+	  x1+(width/2),
+	  y1+(height/2),
+	  x2+(width/2),
+	  y2+(height/2)
+	];
+};
 
 var links = [
   {source: nodes[0], target: nodes[1]},
   {source: nodes[2], target: nodes[1]}
-]
+];
+
+WIDTH = 50;
+HEIGHT = 30;
+
+
 
 vis.selectAll(".line")
    .data(links)
    .enter()
    .append("line")
-   .attr("x1", function(d) { return d.source.x })
-   .attr("y1", function(d) { return d.source.y })
-   .attr("x2", function(d) { return d.target.x })
-   .attr("y2", function(d) { return d.target.y })
+   .attr("x1", function(d) { return getLinkPositions(d.source.x,d.source.y, d.target.x, d.target.y, WIDTH, HEIGHT)[0]; })
+   .attr("y1", function(d) { return getLinkPositions(d.source.x,d.source.y, d.target.x, d.target.y, WIDTH, HEIGHT)[1]; })
+   .attr("x2", function(d) { return getLinkPositions(d.source.x,d.source.y, d.target.x, d.target.y, WIDTH, HEIGHT)[2]; })
+   .attr("y2", function(d) { return getLinkPositions(d.source.x,d.source.y, d.target.x, d.target.y, WIDTH, HEIGHT)[3]; })
    .style("stroke", "rgb(6,120,155)");
 
-vis.selectAll("circle.nodes")
-   .data(nodes)
-   .enter()
-   .append("svg:circle")
-   .attr("cx", function(d) { return d.x; })
-   .attr("cy", function(d) { return d.y; })
-   .attr("r", "10px")
-   .attr("fill", "black")
+var update = function(){
+	vis.selectAll("circle.nodes")
+	   .data(nodes)
+	   .enter()
+	   .append("svg:rect")
+	   .attr("rx", 6)
+	   .attr("ry", 6)
+	   .attr("x", function(d) { return d.x; })
+	   .attr("y", function(d) { return d.y; })
+	   .attr("width", ""+WIDTH+"px")
+	   .attr("height", ""+HEIGHT+"px")
+	   .attr("fill", "purple")
+}
+
+vis.on('click', function() {
+	var coords = d3.mouse(this);
+	nodes.push({x:coords[0], y:coords[1]});
+	update();
+});
+
+update();
