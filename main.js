ee504e157c7a998f55949d1a6c97ec6cf5b8bd04
Panagiotis Pediaditis <panagiotis.pediaditis@citrix.com>
2018-06-01 00:15:34 +0300

    Colour node on click, (find node at mouse)

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-
-
+var WIDTH = 50;
+var HEIGHT = 30;
 
 var vis = d3.select("#graph")
             .append("svg")
 .attr("width", 500).attr("height", 500);
 
 var nodes = [
-    {x: 10, y: 90},
-    {x: 70, y: 10},
-    {x: 140, y: 50}
+    {x: 10, y: 90, c: "purple"},
+    {x: 70, y: 10, c: "purple"},
+    {x: 140, y: 50, c: "purple"}
   ];
 
 
@@ -26,22 +26,19 @@ var links = [
   {source: nodes[2], target: nodes[1]}
 ];
 
-WIDTH = 50;
-HEIGHT = 30;
-
-
-
-vis.selectAll(".line")
-   .data(links)
-   .enter()
-   .append("line")
-   .attr("x1", function(d) { return getLinkPositions(d.source.x,d.source.y, d.target.x, d.target.y, WIDTH, HEIGHT)[0]; })
-   .attr("y1", function(d) { return getLinkPositions(d.source.x,d.source.y, d.target.x, d.target.y, WIDTH, HEIGHT)[1]; })
-   .attr("x2", function(d) { return getLinkPositions(d.source.x,d.source.y, d.target.x, d.target.y, WIDTH, HEIGHT)[2]; })
-   .attr("y2", function(d) { return getLinkPositions(d.source.x,d.source.y, d.target.x, d.target.y, WIDTH, HEIGHT)[3]; })
-   .style("stroke", "rgb(6,120,155)");
 
 var update = function(){
+
+	vis.selectAll(".line")
+       .data(links)
+       .enter()
+       .append("line")
+       .attr("x1", function(d) { return getLinkPositions(d.source.x,d.source.y, d.target.x, d.target.y, WIDTH, HEIGHT)[0]; })
+       .attr("y1", function(d) { return getLinkPositions(d.source.x,d.source.y, d.target.x, d.target.y, WIDTH, HEIGHT)[1]; })
+       .attr("x2", function(d) { return getLinkPositions(d.source.x,d.source.y, d.target.x, d.target.y, WIDTH, HEIGHT)[2]; })
+       .attr("y2", function(d) { return getLinkPositions(d.source.x,d.source.y, d.target.x, d.target.y, WIDTH, HEIGHT)[3]; })
+       .style("stroke", "rgb(6,120,155)");
+
 	vis.selectAll("circle.nodes")
 	   .data(nodes)
 	   .enter()
@@ -52,12 +49,54 @@ var update = function(){
 	   .attr("y", function(d) { return d.y; })
 	   .attr("width", ""+WIDTH+"px")
 	   .attr("height", ""+HEIGHT+"px")
-	   .attr("fill", "purple")
+	   .attr("fill", function(d) { return d.c; });
+
+}
+
+var pointInRectangle = function(p_x, p_y, x, y, width, height ) {
+	if (p_x < x || p_y < y){
+		return false;
+	}
+	if (p_x - width > x || p_y - height > y) {
+		return false;
+	}
+
+	return true;
+}
+
+var nodeAtCoords = function(coords, action, not_found_action) {
+	var found = false;
+	//use traditional loop (we all hate JS)
+	nodes.forEach(function(node){
+		if (pointInRectangle(coords[0], coords[1], node.x, node.y, WIDTH, HEIGHT)) {
+			if(action) action(node);
+			found = true;
+		}
+	});
+
+	if (found == false && not_found_action) not_found_action();
 }
 
 vis.on('click', function() {
 	var coords = d3.mouse(this);
-	nodes.push({x:coords[0], y:coords[1]});
+
+
+	nodeAtCoords(coords,null,
+		                             function(node){
+                                       	nodes.push({x:coords[0], y:coords[1], c:"purple" });
+	                                 });
+	update();
+});
+
+vis.on('mousedown', function(){
+	var coords = d3.mouse(this);
+
+	nodeAtCoords(coords,
+		                             function(node){
+                                       node.c = "green"
+	                                 });
+
+
 	update();
 });
 
